0013ea90b73781afa7a20b88548b960af2f03614
tuanlevan94 <tuanlevan94@gmail.com>
2023-10-07 11:48:46 +0800

    first project

---
+++ assets/JS/overviewjpg.js
@@ -0,0 +1,25 @@
+
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+
+let sections = document.querySelectorAll('section');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            sec.classList.add('show-animate');
+        } //if you want to repeating animation on scroll, use this
+        else{
+            sec.classList.remove('show-animate');
+        }
+    })
+}
